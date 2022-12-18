90db157406005c03148f9bf26614e7abd2dee16c
Konstantinos Koutsopoulos <77823717+WatrGG@users.noreply.github.com>
2022-12-18 15:10:33 -0700

    Add files via upload

---
+++ Header.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdio.h>
+#include <unistd.h>
+// #include <curses.h> for menu gui, might happen next time
+#include <windows.h>
+
+std::string fileName;
+
+
+
